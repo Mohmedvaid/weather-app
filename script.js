7ec841b423468a12679bb75afd31e686d7a4eb2d
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-14 18:41:24 -0500

    append city names fixed.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     var city
     var i = 0;
     var queryURL2;
+    var tempuvindex;
 
     function currentweather() {
         //this will get the current weather
@@ -18,6 +19,7 @@ $(document).ready(function () {
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
+
                 //pushing the data received in the array
                 weatherData.push({
                     name: city,
@@ -26,28 +28,49 @@ $(document).ready(function () {
                     humidity:response.main.humidity,
                     lon: response.coord.lon,
                     lat: response.coord.lat,
-                    uvindex: "",
+                    uvindex: getuv(),
                     // day0: ["temp", "humidity"],
                     // day1: ["temp", "humidity"],
                     // day2: ["temp", "humidity"],
                     // day3: ["temp", "humidity"],
                     // day4: ["temp", "humidity"]
                 })
+
                 //second api call to get the city UV Index
+                
+                function getuv(){
                 queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&units=imperial&lon=${response.coord.lon}&appid=7ac3b8ae4166269284ad86c8653c1b57`
                 $.ajax({
                     url: queryURL2,
                     method: "GET"
                 }).then(function (response2) {
-                    weatherData[i].uvindex = response2.value;
-                    i++;
+
+                     tempuvindex= response2.value; 
+                     console.log(tempuvindex)
+                     return tempuvindex
 
                 })
-                //forecast();
-                //render();
+                
+            }
+           // console.log(weatherData[0].uvindex)
+                
+                $(`.basic-temp`).empty()
+                $(`#city-name`).text(city)
+                $(`.basic-temp`).append(`
+                        <li>Temprature: ${weatherData[i].temprature}</li>
+                        <li>Humidity: ${weatherData[i].humidity}</li>
+                        <li>Wind Speed: ${weatherData[i].windspeed}</li>
+                        <li>UV Index: ${weatherData[i].uvindex} </li>
+        
+                `)
+                render()
+                i++
+                console.log(`LOL`)
+                console.log(`i: ${i}`)
+                console.log(weatherData)
+
             });
-            render();
-            console.log(weatherData)
+            
         })
 
     }
@@ -69,21 +92,18 @@ $(document).ready(function () {
 
     }
 
-
-
-    function render() {
-        //this will display the html
-        $(`#city-name`).text(city)
-        $(`.basic-temp`).append(`
-                <li>Temprature:${weatherData[0].temprature}</li>
-                <li>Humidity:${weatherData[0].humidity}</li>
-                <li>Wind Speed: ${weatherData[0].windspeed}</li>
-                <li>UV Index:${weatherData[0].uvindex} </li>
-
+    function render(){
+        if(weatherData.length>9){
+            $('.list-group li:last-child').remove();
+        }
+       
+        $(`.list-group`).prepend(`
+        <li class="list-group-item">${weatherData[i].name}</li>
         `)
     }
 
     currentweather();
+    //render();
     
 
 
