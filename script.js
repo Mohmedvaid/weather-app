35892f15092272b4d39e7cb5f8a4e17d258af7ce
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-13 23:20:03 -0500

    Added basic logic

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ $(document).ready(function () {
         $(`#city-btn`).on('click', function () {
             //user value is saved in the city
             city = $(".form-control").val();
-            queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7ac3b8ae4166269284ad86c8653c1b57`
-            console.log(queryURL)
+            queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7ac3b8ae4166269284ad86c8653c1b57`
             //api call to get the current weather
             $.ajax({
                 url: queryURL,
@@ -34,7 +33,7 @@ $(document).ready(function () {
                     // day4: ["temp", "humidity"]
                 })
                 //second api call to get the city UV Index
-                queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=7ac3b8ae4166269284ad86c8653c1b57`
+                queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&units=imperial&lon=${response.coord.lon}&appid=7ac3b8ae4166269284ad86c8653c1b57`
                 $.ajax({
                     url: queryURL2,
                     method: "GET"
@@ -45,6 +44,7 @@ $(document).ready(function () {
                 })
             });
             forecast();
+             render();
 
             console.log(weatherData)
         })
@@ -54,14 +54,15 @@ $(document).ready(function () {
 
     function forecast() {
         //this will get the 5 day forecase
-        var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7ac3b8ae4166269284ad86c8653c1b57`
+        var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=7ac3b8ae4166269284ad86c8653c1b57`
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
 
             for (var j = 0; j < 5; j++) {
-                weatherData[i][`day${j}`] = [response.list[j].main.temp, response.list[j].main.humidity]
+
+                weatherData[i][`day${j}`] = [response.list[j].main.temp, response.list[j].main.humidity,response.list[j].dt_txt]
             }
         })
 
@@ -71,10 +72,18 @@ $(document).ready(function () {
 
     function render() {
         //this will display the html
+        $(`#city-name`).text(city)
+        $(`.basic-temp`).append(`
+                <li>Temprature:${weatherData[0].temprature}</li>
+                <li>Humidity:${weatherData[0].humidity}</li>
+                <li>Wind Speed: </li>
+                <li>UV Index: </li>
+
+        `)
     }
 
     currentweather();
-    console.log(weatherData)
+    
 
 
 
