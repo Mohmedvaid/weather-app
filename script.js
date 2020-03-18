23b6a1c5d307f5dcc8f6d32503f95d1a9f3e127a
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-03-17 21:14:30 -0500

    Cleared console logs, added comments, fixed UV index and added comments

--- script.js
+++ script.js
@@ -6,35 +6,42 @@ $(document).ready(function () {
     var city
     var i;
     var queryURL2;
-    var tempuvindex;
+    var iconimg
+
+
 
 
     function currentweather() {
         //this will get the current weather
 
-        if(JSON.parse(localStorage.getItem('weatherData'))){
+        //this will check if weatherData array is in the local storage 
+        if (JSON.parse(localStorage.getItem('weatherData'))) {
             weatherData = JSON.parse(localStorage.getItem('weatherData'))
             i = weatherData.length;
-            for(var j = 0; j<weatherData.length; j++){
+            for (var j = 0; j < weatherData.length; j++) {
                 $(`.list-group`).prepend(`
                 <li class="list-group-item">${weatherData[j].name}</li>
                 `)
-
             }
-        } else{
-            i=0;
+        } else {
+            i = 0;
         }
 
 
+
+        // this will get current weather of the city entered by the user
         $(`#city-btn`).on('click', function () {
             //user value is saved in the city
             city = $(".form-control").val();
             queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7ac3b8ae4166269284ad86c8653c1b57`
+            
             //api call to get the current weather
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
+                tempresponse = response;
+                iconimg = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
 
                 //pushing the data received in the array
                 weatherData.push({
@@ -44,7 +51,7 @@ $(document).ready(function () {
                     humidity: response.main.humidity,
                     lon: response.coord.lon,
                     lat: response.coord.lat,
-                    uvindex: getuv(),
+                    uvindex: "test",
                     day0: ["temp", "humidity"],
                     day1: ["temp", "humidity"],
                     day2: ["temp", "humidity"],
@@ -52,55 +59,43 @@ $(document).ready(function () {
                     day4: ["temp", "humidity"]
                 })
 
-                //second api call to get the city UV Index
-
-                function getuv() {
-                    queryURL2 = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&units=imperial&lon=${response.coord.lon}&appid=7ac3b8ae4166269284ad86c8653c1b57`
-                    $.ajax({
-                        url: queryURL2,
-                        method: "GET"
-                    }).then(function (response2) {
-
-                        tempuvindex = response2.value;
-                    
-                        return tempuvindex
-
-                    })
-
-                }
-
-
+                //this will display the data received from api call
+                
+                $(`#main-img`).attr(`src`,iconimg).attr(`style`, `display: block`)
                 $(`.basic-temp`).empty()
                 $(`#city-name`).text(city)
                 $(`.basic-temp`).append(`
                         <li>Temprature: ${weatherData[i].temprature}</li>
                         <li>Humidity: ${weatherData[i].humidity}</li>
                         <li>Wind Speed: ${weatherData[i].windspeed}</li>
-                        <li>UV Index: ${weatherData[i].uvindex} </li>
+                        <li id="uv-index">UV Index: <p id="uv-num"> ${weatherData[i].uvindex}</p></li>
         
                 `)
-                ////forecast()
-                var queryURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=e504647e199d4c37b3d24db5eab9b660&units=i`
-                
-                $(`.cards`).attr(`style`, `display: flex`);
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).then(function (response) {
 
-                    for (var j = 1; j < 7; j++) {
-                        $(`#card${j}`).text(response.data[j].datetime)
-                        $(`#img${j}`).attr(`src`, `https://www.weatherbit.io/static/img/icons/${response.data[j].weather.icon}.png`)
-                        $(`#card-temp${j}`).text(`Temp: ${response.data[j].temp} F`)
-                        $(`#card-hum${j}`).text(`Humidity: ${response.data[j].rh}%`)
-                        
+                //second api call to get the city UV Index
+                queryURL2 = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&units=imperial&lon=${response.coord.lon}&appid=7ac3b8ae4166269284ad86c8653c1b57`
+                $.ajax({
+                    url: queryURL2,
+                    method: "GET"
+
+                }).then(function (response2) {
+                    $(`#uv-num`).text(` ${response2.value}`)
+
+                    if(response2.value <4){
+                        $(`#uv-num`).attr(`class`, `bg-success`)
+                    }
+                    else if (response2.value<7){
+                        $(`#uv-num`).attr(`class`, `bg-warning`)
+                    }
+                    else{
+                        $(`#uv-num`).attr(`class`, `bg-danger`)
                     }
                 })
+                forecast(city)
                 render()
                 i++
+                // adding the data to local storage
                 localStorage.setItem('weatherData', JSON.stringify(weatherData));
-                
-
             });
 
         })
@@ -111,15 +106,18 @@ $(document).ready(function () {
     function forecast() {
         //this will get the 5 day forecase
         var queryURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=e504647e199d4c37b3d24db5eab9b660&units=i`
-       
+
+        $(`.cards`).attr(`style`, `display: flex`);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
 
-            for (var j = 1; j < 6; j++) {
-
-                weatherData[i][`day${j}`] = [response.data[j].high_temp, response.data[j].rh]
+            for (var j = 1; j < 7; j++) {
+                $(`#card${j}`).text(response.data[j].datetime)
+                $(`#img${j}`).attr(`src`, `https://www.weatherbit.io/static/img/icons/${response.data[j].weather.icon}.png`)
+                $(`#card-temp${j}`).text(`Temp: ${response.data[j].temp} F`)
+                $(`#card-hum${j}`).text(`Humidity: ${response.data[j].rh}%`)
 
             }
         })
@@ -127,25 +125,20 @@ $(document).ready(function () {
     }
 
     function render() {
+        // this will make sure that number of cities on left do not exceed 9
         if (weatherData.length > 9) {
             $('.list-group li:last-child').remove();
             weatherData.shift();
-            i = weatherData.length -1;
-            console.log(weatherData)
+            i = weatherData.length - 1;
         }
-        
+
         $(`.list-group`).prepend(`
         <li class="list-group-item">${weatherData[i].name}</li>
         `)
     }
 
+    
     currentweather();
-
-
-
-
-
-
 
     //ready ends
 });
